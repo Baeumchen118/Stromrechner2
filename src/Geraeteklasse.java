9b3d8a80c6698e9c1143ccfff6e778eeb99b2522
Thomas Kleebaum <tkleebau@hs-mittweida.de>
2015-07-09 11:49:37 +0200

    Geräteklasse und Testklasse erstellt

---
+++ src/Geraeteklasse.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Thomas Kleebaum-Nagy on 09.07.2015.
+ */
+public class Geraeteklasse {
+    int klassenId;
+    String klassenName;
+
+//    Standardkonstruktor
+    public Geraeteklasse(){
+
+    }
+
+    public Geraeteklasse(int klassenId, String klassenName){
+        this.klassenId = klassenId;
+        this.klassenName = klassenName;
+    }
+
+}
---
+++ src/Tester.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Thomas Kleebaum-Nagy on 09.07.2015.
+ */
+public class Tester {
+    public static void main(String args){
+        Geraeteklasse gerae1 = new Geraeteklasse(2504, "Leuchtmittel");
+        System.out.println(gerae1.klassenName);
+
+
+    }
+}
